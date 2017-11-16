a020401761863a02547fa90d5b954ac813c6c4a2
Artisan du Numérique <etienne@artisan-numerique.ch>
2017-11-16 14:33:00 +0100

    Mouse event handling helper

---
+++ widgets/_Mouse.js
@@ -0,0 +1,41 @@
+define([
+	"dojo/_base/declare",
+	"dojo/_base/lang"
+], function(
+	djDeclare,
+	djLang
+){
+
+var _Mouse = djDeclare(null, {
+
+	getAbsPos: function(event) {
+		console.log(event);
+
+		if(event.pageX && event.pageY) {
+			return {x : event.pageX, y: event.pageY };	
+		} else {
+			return this.getTargetAbsPos(event);	
+		}
+	},
+
+	getTargetAbsPos: function (event) {
+		var x = 0, y = 0, el = event.target;
+
+		while(el) {
+			if(el.tagName == 'BODY') {
+				x += el.offsetLeft - (el.scrollLeft || document.documentElement.scrollLeft) ;
+				y += el.offsetTop - (el.scrollTop || document.documentElement.scrollTop) ;
+			}	else {
+				x += el.offsetLeft - el.scrollLeft;
+				y += el.offsetTop - el.scrollTop;
+			}
+			el = el.offsetParent;
+		}
+
+		return { x: x, y: y};
+	}
+	
+});
+	
+return _Mouse;
+});
