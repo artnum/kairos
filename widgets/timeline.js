artnum/kairos

commit 4d32ad5dffd5aaa77239976ead53f8e0ca43ff93

Begin and end filter can be set by date

widgets/timeline.js
@@ -26,6 +26,7 @@ define([
 	"dojo/window",
 	"dojo/promise/all",
 	"dijit/registry",
+	"dijit/form/DateTextBox",
 	"dijit/form/NumberTextBox",
 	"dijit/form/Button",
 	"dijit/MenuBar",
@@ -76,6 +77,7 @@ define([
 	djWindow,
 	djAll,
 	dtRegistry,
+	dtDateTextBox,
 	dtNumberTextBox,
 	dtButton,
 	dtMenuBar,
@@ -534,20 +536,16 @@ return djDeclare("location.timeline", [
 				}
 
 				that.searchMenu.addChild(new dtMenuSeparator());
-
-				var item = new dtMenuItem({ label: 'Commence demain' });
-				djOn(item, 'click', djLang.hitch(that, () => {
-					var now = new Date();
-					now = djDate.add(now, 'day', 1);
-					that.filterDate(now);
+				var now = djDateStamp.toISOString(djDate.add(new Date(), 'day', 1));
+				var item = new dtMenuItem({ label: 'Comment le <input data-dojo-type="dijit/form/DateTextBox" id="menuStartDay" value="' + now + '" />' });
+				djOn(item, 'click', djLang.hitch(that, (e) => {
+					that.filterDate(dtRegistry.byId('menuStartDay').get('value'));
 				}));
 				that.searchMenu.addChild(item);
 				
-				var item = new dtMenuItem({ label: 'Termine demain' });
+				var item = new dtMenuItem({ label: 'Termine le <input data-dojo-type="dijit/form/DateTextBox" id="menuEndDay" value="' + now + '" />' });
 				djOn(item, 'click', djLang.hitch(that, () => {
-					var now = new Date();
-					now = djDate.add(now, 'day', 1);
-					that.filterDate(now, 'trueEnd');
+					that.filterDate(dtRegistry.byId('menuEndDay').get('value'), 'trueEnd');
 				}));
 				that.searchMenu.addChild(item);
 

commit be40dd3cef01aa2df9c6b8e24331169947e41074

Fix stange bug introduced in 4d32ad5dffd5aaa77239976ead53f8e0ca43ff93

widgets/timeline.js
@@ -465,7 +465,6 @@ return djDeclare("location.timeline", [
 
 	buildMenu: function() {
 		var that = this;
-		this.menu.startup();
 
 		var item = new dtMenuItem({ label: 'Tout', disabled: true});
 		djOn(item, 'click', djLang.hitch(that, that.filterNone));
@@ -536,23 +535,29 @@ return djDeclare("location.timeline", [
 				}
 
 				that.searchMenu.addChild(new dtMenuSeparator());
+
 				var now = djDateStamp.toISOString(djDate.add(new Date(), 'day', 1));
-				var item = new dtMenuItem({ label: 'Commence le <input data-dojo-type="dijit/form/DateTextBox" id="menuStartDay" value="' + now + '" />' });
+
+				var item = new dtMenuItem({ label: 'Commence le '  });
+				var x = new dtDateTextBox({ value: now, id: 'menuStartDay' });
+				item.containerNode.appendChild(x.domNode);
+				item.own(x);
 				djOn(item, 'click', djLang.hitch(that, (e) => {
 					that.filterDate(dtRegistry.byId('menuStartDay').get('value'));
 				}));
 				that.searchMenu.addChild(item);
-				item.startup()
 				
-				var item = new dtMenuItem({ label: 'Termine le <input data-dojo-type="dijit/form/DateTextBox" id="menuEndDay" value="' + now + '" />' });
+				var item = new dtMenuItem({ label: 'Termine le '});
 				djOn(item, 'click', djLang.hitch(that, () => {
 					that.filterDate(dtRegistry.byId('menuEndDay').get('value'), 'trueEnd');
 				}));
+				var x = new dtDateTextBox({ value: now, id: 'menuEndDay' });
+				item.containerNode.appendChild(x.domNode);
+				item.own(x);
 				that.searchMenu.addChild(item);
-				item.startup()
 
 
-			});
+			}).then(() => { that.menu.startup(); });
 		});
 	},
 