a58e5faa2fd41029eb338c7181294d06bc51ec7a
artnum <etienne@artisan-numerique.ch>
2018-05-07 08:41:38 +0200

    Association change update mtime of a reservation

--- store/Models/AssociationModel.php
+++ store/Models/AssociationModel.php
@@ -4,5 +4,22 @@ class AssociationModel extends artnum\SQL {
       parent::__construct($db, 'association', 'association_id', $config);
       $this->conf('auto-increment', true);
    }
+
+   function write($data) {
+      $results = parent::write($data);
+      if($results) {
+         $now = new \DateTime('now', new \DateTimeZone('UTC')); $now = $now->format('c');
+         try {
+            $st = $this->DB->prepare('UPDATE reservation SET reservation_modification = :time WHERE reservation_id = :target');
+            $st->bindParam(':time', $now, \PDO::PARAM_STR);
+            $st->bindParam(':target', $data['reservation'], \PDO::PARAM_INT);
+            $st->execute();
+         } catch (\Exception $e) {
+            /* nothing */
+         }
+      }
+
+      return $results;
+   }
 }
 ?>
