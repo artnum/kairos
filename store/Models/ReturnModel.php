d0700606cc361f58ca458b3af7810cb67fae34ea
artnum <etienne@artisan-numerique.ch>
2018-06-19 11:21:40 +0200

    Modification timestamp for "return" table

---
+++ store/Models/ReturnModel.php
@@ -0,0 +1,10 @@
+<?PHP
+class ReturnModel extends artnum\SQL {
+   function __construct($db, $config) {
+      parent::__construct($db, 'return', 'return_id', $config);
+      $this->conf('auto-increment', true);
+      $this->conf('mtime', 'return_modification');
+      $this->conf('mtime.ts', true);
+   }
+}
+?>
