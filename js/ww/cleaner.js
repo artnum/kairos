4182cfd9ed7b1b08aed1ca04422bedfd9158c07b
artnum <etienne@artisan-numerique.ch>
2018-06-28 13:53:33 +0200

    Avoid stale deleted value in IndexedDB

--- js/ww/cleaner.js
+++ js/ww/cleaner.js
@@ -1,42 +1,55 @@
 /* eslint-env worker */
-/* global wantdb, DB */
+/* global IdxDB */
 'use strict'
 importScripts('../localdb.js')
 
-wantdb(cleaner)
+new IdxDB().then(function (DB) {
+  function cleaner () {
+    var store = DB.transaction('reservations', 'readwrite').objectStore('reservations')
+    var cleaned = []
 
-function cleaner () {
-  var store = DB.transaction('reservations', 'readwrite').objectStore('reservations')
-  var cleaned = []
+    store.openCursor().onsuccess = function (event) {
+      var cursor = event.target.result
 
-  store.openCursor().onsuccess = function (event) {
-    var cursor = event.target.result
-
-    if (!cursor) {
-      postMessage(cleaned)
-      setTimeout(cleaner, 2500)
-      return
-    }
-    var value = cursor.value
-    var del = false
-
-    if (typeof value.id !== 'string') {
-      cursor.delete()
-      del = true
-    }
-
-    if (value.deleted != null) {
-      if (cleaned.indexOf(value.target) === -1) {
-        cleaned.push(value.target)
+      if (!cursor) {
+        postMessage(cleaned)
+        setTimeout(cleaner, 2500)
+        return
       }
-      del = true
-      cursor.delete()
-    }
+      var value = cursor.value
+      var del = false
 
-    if (del && value.return) {
-      DB.transaction('return', 'readwrite').objectStore('return').delete(value.return.id)
-    }
+      if (typeof value.id !== 'string') {
+        cursor.delete()
+        del = true
+      }
 
-    cursor.continue()
+      if (!del) {
+        fetch('/location/store/Reservation/' + value.id, {method: 'head'}).then(function (response) {
+          var deleted = response.headers.get('X-Artnum-deleted')
+          if (Number(deleted)) {
+            DB.transaction('reservations', 'readwrite').objectStore('reservations').delete(value.id)
+          }
+        }, function (error) {
+          console.error('Error fetch reservation', error)
+        })
+      }
+
+      if (value.deleted != null) {
+        if (cleaned.indexOf(value.target) === -1) {
+          cleaned.push(value.target)
+        }
+        del = true
+        cursor.delete()
+      }
+
+      if (del && value.return) {
+        DB.transaction('return', 'readwrite').objectStore('return').delete(value.return.id)
+      }
+
+      cursor.continue()
+    }
   }
-}
+
+  cleaner()
+})
