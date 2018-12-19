4593338eadbed92adf5e5a74bfb55dfc8df29104
artnum <etienne@artisan-numerique.ch>
2018-12-19 12:07:55 +0100

    Improve "revision" detection

--- revision.php
+++ revision.php
@@ -1,8 +1,24 @@
 <?PHP
-   header('Cache-Control: no-cache; max-age=0');
+/* Hash functions don't need to be secure and collision are not revelant */
+header('Cache-Control: no-cache; max-age=0');
+
+if (is_file('private/VERSION') && is_readable('private/VERSION')) {
+   $version = file_get_contents('VERSION');
+} else {
    exec('git rev-parse --verify HEAD', $output, $retval);
 
    if (intval($retval) == 0) {
-      echo '{ "revision": "'. $output[0]. '" }'; 
+      $version = $output[0];
+   } else {
+      $version = 'unknown version';
    }
+}
+
+$key = 'unconfigured key';
+if (is_file('conf/location.ini') && is_readable('conf/location.ini')) {
+   $key = hash_file('md5', 'conf/location.ini');
+}
+$revision = hash_hmac('md5', $version, $key);
+
+echo '{ "revision": "'. $revision . '" }';
 ?>
