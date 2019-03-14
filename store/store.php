b82048dbcd2e16904f25572f2963fd13fd568fdc
artnum <etienne@artisan-numerique.ch>
2019-03-14 09:19:13 +0100

    Reorder code to avoid initializing useless parts

--- store/store.php
+++ store/store.php
@@ -12,21 +12,18 @@ if (!isset($ini_conf['general']['disable-locking'])) {
 }
 
 $file = new \artnum\Files();
-$rand = new \artnum\Random();
 $http_request = new \artnum\HTTP\JsonRequest();
 $store = new \artnum\JStore\Generic($http_request, true);
 
-$lock_pdo = new PDO($ini_conf['lock']['pdo-string'], $ini_conf['lock']['user'], $ini_conf['lock']['password']);
-$lock = new \artnum\Lock(array('dbtype'=>'pdo', 'db'=>$lock_pdo));
-
 $sigfile = getcwd() . '/../private/sig.txt';
 if (!file_exists($sigfile) && $file->writable($sigfile)) {
+   $rand = new \artnum\Random();
    $rand->str(512, $sigfile);
 } else if (!$file->readable($sigfile)) {
    throw new Exception('No signature file');
 }
 
-$pdo_db = new PDO($ini_conf['storage']['pdo-string'], $ini_conf['storage']['user'], $ini_conf['storage']['password']);
+$pdo_db = new PDO($ini_conf['storage']['pdo-string'], $ini_conf['storage']['user'], $ini_conf['storage']['password'], array(PDO::ATTR_PERSISTENT => true));
 $pdo_db->exec('SET sql_mode=\'ANSI\';');
 $ldap_db = new artnum\LDAPDB(
          array(
@@ -39,6 +36,8 @@ $ldap_db = new artnum\LDAPDB(
       );
 
 if (!$ini_conf['general']['disable-locking']) {
+   $lock_pdo = new PDO($ini_conf['lock']['pdo-string'], $ini_conf['lock']['user'], $ini_conf['lock']['password'], array(PDO::ATTR_PERSISTENT => true));
+   $lock = new \artnum\Lock(array('dbtype'=>'pdo', 'db'=>$lock_pdo));
    $store->set('lockmanager', $lock);
 } else {
    $store->set('lockmanager', 'void');
