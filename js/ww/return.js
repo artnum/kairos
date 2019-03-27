4453dff87461722521d0040d2cbc8044cbf149ff
artnum <etienne@artisan-numerique.ch>
2019-03-27 17:09:33 +0100

    Speed up

--- js/ww/return.js
+++ js/ww/return.js
@@ -1,104 +1,27 @@
 /* eslint-env worker */
 /* global IdxDB, Artnum */
 'use strict'
-
-importScripts('../localdb.js')
+importScripts('https://bitwiseshiftleft.github.io/sjcl/sjcl.js')
 importScripts('https://artnum.ch/code/js/Path.js')
+importScripts('https://artnum.ch/code/js/Query.js')
 
-var findLastMod = function (db) {
-  return new Promise(function (resolve, reject) {
-    var st = db.transaction('return', 'readwrite').objectStore('return')
-    var lastmod = 0
-
-    st.openCursor().onsuccess = function (event) {
-      var cursor = event.target.result
-
-      if (!cursor) {
-        resolve(lastmod)
-        return
-      }
-
-      if (cursor.value.deleted) {
-        cursor.continue()
-        return
-      }
-
-      var ts = parseInt(cursor.value.modification)
-      if (ts < Math.round((new Date()).getTime() / 1000) && lastmod < ts) {
-        lastmod = ts
-      }
-      cursor.continue()
-    }
-  })
-}
-
-var fetchLastMod = function (lastmod, db) {
-  var url = Artnum.Path.url('store/Arrival')
-  url.searchParams.append('long', '1')
-  url.searchParams.append('search.modification', '>' + lastmod)
-  fetch(url).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (values) {
-        if (values.type === 'results') {
-          var newest = lastmod
-          var st = db.transaction('return', 'readwrite').objectStore('return')
-          values.data.forEach((e) => {
-            postMessage(e)
-            if (e.deleted) {
-              if (st.getKey(e.id)) {
-                st.delete(e.id)
-              }
-            } else {
-              if (e.modification > Math.round((new Date()).getTime() / 1000)) {
-                e.modification = Math.round((new Date()).getTime() / 1000)
-              }
-              st.put(e)
-            }
-            var ts = parseInt(e.modification)
-            if (ts > newest) {
-              newest = ts
-            }
-          })
-          fetchLastMod(String(newest), db)
-        }
-      })
-    }
-  })
-}
-
-function clean (db) {
-  var st = db.transaction('return').objectStore('return')
-  st.getAllKeys().onsuccess = function (event) {
-    var keys = event.target.result
-
-    /* this is done to limit size of URL */
-    do {
-      var subkeys = keys.splice(0, 200)
-      var strkeys = subkeys.join('|')
-      fetch(Artnum.Path.url('/store/Arrival/|' + strkeys)).then(function (response) {
-        response.json().then(function (data) {
-          var entries = data.data
-          var found = []
-          st = db.transaction('return', 'readwrite').objectStore('return')
-          for (var i = 0; i < entries.length; i++) {
-            if (entries[i].deleted === '') { entries[i].deleted = null }
-            if (entries[i].done === '') { entries[i].done = null }
-
-            if (entries[i].deleted != null || entries[i].done != null) {
-              st.delete(entries[i].id)
-              postMessage({delete: true, id: entries[i].id})
-            } else {
-              found.push(entries[i].id)
-            }
-          }
-        })
-      })
-    } while (keys.length > 0)
-    setTimeout(function () { clean(db) }, 15000)
+var lastMod = 0
+function run () {
+  var p = {'search.done': '-'}
+  if (lastMod > 0) {
+    p = {'search.modification': `>${lastMod}`, 'long': '1'}
   }
+  Artnum.Query.exec(Artnum.Path.url('store/Arrival', {params: p})).then(function (results) {
+    console.log(results)
+    if (results.success && results.length > 0) {
+      for (var i = 0; i < results.length; i++) {
+        postMessage(results.data[i])
+        var mod = parseInt(results.data[i].modification)
+        if (mod > lastMod) { lastMod = mod }
+      }
+    }
+    setTimeout(run, lastMod > 0 ? 1000 : 1000)
+  })
 }
 
-new IdxDB().then(function (db) {
-  findLastMod(db).then(function (lastmod) { fetchLastMod(lastmod, db) })
-  setTimeout(function () { clean(db) }, 15000)
-})
+run()
