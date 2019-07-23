6a822b27e23483d8e09071622d777ddde6e26ab0
artnum <etienne@artisan-numerique.ch>
2019-07-23 11:42:15 +0200

    Ajout de la base pour les codes postaux

---
+++ resources/post-codes/update-post-codes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://www.bfs.admin.ch/bfsstatic/dam/assets/275785/master -O raw/post-codes.xls
+ssconvert -S raw/post-codes.xls raw/post-codes.csv
+cp raw/post-codes.csv.1 post-codes.csv
+
