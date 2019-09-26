8d9babc36b0b470d35586d661a0c8ae0e8362a9f
artnum <etienne@artisan-numerique.ch>
2019-09-26 16:49:48 +0200

    Changer la vue pour exclure correctement les décomptes finaux

--- docs/SQL/Views.sql
+++ docs/SQL/Views.sql
@@ -5,6 +5,13 @@ DROP VIEW IF EXISTS "firstContactClient";
 CREATE VIEW "firstContactClient" AS
        SELECT * FROM "contacts" WHERE "contacts_comment" = '_client' GROUP BY "contacts_reservation";
 
+DROP VIEW IF EXISTS "reservationWithFinalCount";
+CREATE VIEW "reservationWithFinalCount" AS
+       SELECT "countReservation_reservation" FROM "count"
+       	      LEFT JOIN "countReservation" ON "count_id" = "countReservation_count"
+       WHERE "count_state" = 'FINAL'
+       GROUP BY "countReservation_reservation";
+
 DROP VIEW IF EXISTS "uncounted";
 CREATE VIEW "uncounted" AS
        SELECT
@@ -24,5 +31,5 @@ CREATE VIEW "uncounted" AS
 	     LEFT JOIN "user" AS "creator" ON "creator"."user_id" = REVERSE(SUBSTR(REVERSE("reservation"."reservation_creator"), 1, LOCATE('/', REVERSE("reservation"."reservation_creator")) - 1))
 	     LEFT JOIN "user" AS "technician" ON "technician"."user_id" = REVERSE(SUBSTR(REVERSE("reservation"."reservation_technician"), 1, LOCATE('/', REVERSE("reservation"."reservation_technician")) - 1))
 	     LEFT JOIN "firstContactClient" AS contact ON "reservation_id" = "contact"."contacts_reservation"
-	WHERE "reservation_deleted" IS NULL AND "reservation_closed" IS NULL AND ("count_state" != 'FINAL' OR "countReservation_id" IS NULL)
+	WHERE "reservation_deleted" IS NULL AND "reservation_closed" IS NULL AND ("reservation_id" NOT IN (SELECT * FROM reservationWithFinalCount) OR "countReservation_id" IS NULL)
 	GROUP BY "reservation_id";
