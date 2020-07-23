ee4d8449710165e8a896df394bd190f3d8cae583
artnum <etienne@artisan-numerique.ch>
2020-07-23 16:36:47 +0200

    Limit field for users (because  binary aes key make php crash silently for no reason)

--- store/Models/UserModel.php
+++ store/Models/UserModel.php
@@ -2,6 +2,7 @@
    class UserModel extends artnum\SQL {
       function __construct($db, $config) {
          parent::__construct($db, 'user', 'user_id', $config);
+         $this->set_req('get', 'SELECT "user_id", "user_name", "user_nick", "user_phone", "user_disabled", "user_color", "user_function", "user_temporary" FROM "\\Table"');
          $this->conf('auto-increment', true);
       }
    }
