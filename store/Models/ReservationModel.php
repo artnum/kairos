296af315ba9b62ea00f76864f83dcf892b513bee
artnum <etienne@artisan-numerique.ch>
2020-12-21 16:07:51 +0100

    Close #192

--- store/Models/ReservationModel.php
+++ store/Models/ReservationModel.php
@@ -23,5 +23,63 @@ class ReservationModel extends artnum\SQL {
          'reservation_address' => 'string'
       ));
    }
+
+   function getStats($options) {
+      $result = new \artnum\JStore\Result();
+      if (empty($options['from'])) {
+         $from = new DateTime('now');
+         $from->setDate($from->format('y'), 1, 1);
+         $from->setTime(0, 0, 0, 0);
+         $options['from'] = $from->format('U');
+      } else {
+         $from = new DateTime($options['from']);
+         $options['from'] = $from->format('U');
+      }
+      if (empty($options['to'])) {
+         $to = new DateTime('now');
+         $options['to'] = $to->format('U');
+      } else {
+         $to = new DateTime($options['to']);
+         $options['to'] = $to->format('U');
+      }
+      
+      /* SELECT "reservation_creator",
+             COUNT("reservation_id") AS "reservation_totUser",
+             MIN("reservation_created") AS "reservation_first",
+             MAX("reservation_created") AS "reservation_last", 
+            (SELECT COUNT("reservation_id") FROM "reservation" WHERE "reservation_created" >=  1577836800  AND "reservation_created" <= 1608545832 AND COALESCE("reservation_deleted", 0) = 0) AS reservation_total
+         FROM "reservation"
+         WHERE "reservation_created" >=  1577836800  AND "reservation_created" <= 1608545832 AND COALESCE("reservation_deleted", 0) = 0 
+         GROUP BY idFromUrl(reservation_creator) 
+         ORDER BY "reservation_totUser" DESC;
+       */
+      $query = 'SELECT idFromUrl("reservation_creator") AS "reservation_id", "reservation_creator", COUNT("reservation_id") AS "reservation_totUser",
+                  (SELECT COUNT("reservation_id") FROM "reservation" WHERE "reservation_created" >= :from AND "reservation_created" <= :to AND COALESCE("reservation_deleted", 0) = 0) AS "reservation_total"
+                  FROM "reservation"  
+                  WHERE "reservation_created" >= :from AND "reservation_created" <= :to AND COALESCE("reservation_deleted", 0) = 0 __USER__
+                  GROUP BY idFromUrl(reservation_creator)
+                  ORDER BY "reservation_totUser" DESC';
+
+      if (empty($options['user'])) {
+         $query = str_replace('__USER__', '', $query);
+      } else {
+         $query = str_replace('__USER__', ' AND idFromUrl("reservation_creator") = :user', $query);
+      }
+
+      $stmt = $this->get_db(true)->prepare($query);
+      $stmt->bindParam(':from', $options['from'], PDO::PARAM_INT);
+      $stmt->bindParam(':to', $options['to'], PDO::PARAM_INT);
+      if (!empty($options['user'])) {
+         $stmt->bindParam(':user', $options['user'], PDO::PARAM_INT);
+      }
+
+      if ($stmt->execute()) {
+         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $result->addItem($this->unprefix($row));
+         }
+      }
+      return $result;
+   }
+
 }
 ?>
